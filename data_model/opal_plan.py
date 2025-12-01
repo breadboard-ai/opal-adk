bfe20b92aa918fb73d74332366088842d33f06af
Brian Walker <bdwalker@google.com>
2025-12-01 10:30:31 -0800

    Adds a dataclass to capture the graph structure on the
    PiperOrigin-RevId: 838835806

---
+++ data_model/opal_plan.py
@@ -0,0 +1,70 @@
+"""Data model for an Opal plan.
+
+An OpalPlan is a collection of steps that define a sequence of operations
+to be performed.
+"""
+
+import dataclasses
+import textwrap
+from opal_adk.data_model import opal_plan_step
+
+
+@dataclasses.dataclass
+class OpalPlan:
+  """Represents a plan in an e2e Opal plan.
+
+  An OpalPlan is a collection of steps that define a sequence of operations
+  to be performed. Each step is typically a single Node in the Opal graph. A
+  plan may contain a list of steps or a list of lists of steps. The latter is
+  used to represent a plan that contains a list of steps that should be
+  executed in parallel. Any plan step after the parrallel steps should be
+  executed after all the parallel steps have completed.
+  """
+
+  plan_name: str
+  plan_steps: list[
+      opal_plan_step.OpalPlanStep | list[opal_plan_step.OpalPlanStep]
+  ]
+
+  @property
+  def render(self) -> str:
+    """Renders the OpalPlan as a string."""
+
+    def _render_step(
+        step: opal_plan_step.OpalPlanStep, indent: int
+    ) -> list[str]:
+      """Renders a single step with the given indentation level."""
+      raw_lines = step.render().strip().splitlines()
+      if not raw_lines:
+        return []
+
+      rendered_lines = []
+      rendered_lines.append(' ' * indent + raw_lines[0])
+
+      inner_indent = ' ' * (indent + 2)
+      for line in raw_lines[1:-1]:
+        rendered_lines.append(inner_indent + line)
+
+      if len(raw_lines) > 1:
+        rendered_lines.append(' ' * indent + raw_lines[-1])
+
+      return rendered_lines
+
+    lines = []
+    lines.append('<plan>')
+    lines.append(f'  <plan_name>{self.plan_name}</plan_name>')
+    lines.append('  <plan_steps>')
+
+    for step_or_parallel_list in self.plan_steps:
+      if isinstance(step_or_parallel_list, list):
+        lines.append('    <parallel>')
+        for step in step_or_parallel_list:
+          lines.extend(_render_step(step, indent=6))
+        lines.append('    </parallel>')
+      else:
+        lines.extend(_render_step(step_or_parallel_list, indent=4))
+
+    lines.append('  </plan_steps>')
+    lines.append('</plan>')
+
+    return '\n'.join(lines)
---
+++ data_model/opal_plan_test.py
@@ -0,0 +1,191 @@
+"""Tests for opal_plan."""
+
+from absl.testing import absltest
+from absl.testing import parameterized
+from opal_adk.data_model import opal_plan
+from opal_adk.data_model import opal_plan_step
+
+
+class OpalPlanTest(parameterized.TestCase):
+
+  @parameterized.named_parameters(
+      (
+          'single_step',
+          [
+              opal_plan_step.OpalPlanStep(
+                  step_name='step1',
+                  step_intent='intent1',
+                  model_api='api1',
+                  input_parameters=['param1'],
+                  output='output1',
+                  reasoning='reasoning1',
+              )
+          ],
+          [
+              '<plan>',
+              '  <plan_name>test_plan</plan_name>',
+              '  <plan_steps>',
+              '    <plan_step>',
+              '      <step_name>step1</step_name>',
+              '      <step_intent>intent1</step_intent>',
+              '      <system_prompt></system_prompt>',
+              '      <model_api>api1</model_api>',
+              "      <input_parameters>['param1']</input_parameters>",
+              '      <output>output1</output>',
+              '      <reasoning>reasoning1</reasoning>',
+              '      <is_list_output>False</is_list_output>',
+              '    </plan_step>',
+              '  </plan_steps>',
+              '</plan>',
+          ],
+      ),
+      (
+          'parallel_steps',
+          [[
+              opal_plan_step.OpalPlanStep(
+                  step_name='step1',
+                  step_intent='intent1',
+                  model_api='api1',
+                  input_parameters=['param1'],
+                  output='output1',
+                  reasoning='reasoning1',
+              ),
+              opal_plan_step.OpalPlanStep(
+                  step_name='step2',
+                  step_intent='intent2',
+                  model_api='api2',
+                  input_parameters=['param2'],
+                  output='output2',
+                  reasoning='reasoning2',
+              ),
+          ]],
+          [
+              '<plan>',
+              '  <plan_name>test_plan</plan_name>',
+              '  <plan_steps>',
+              '    <parallel>',
+              '      <plan_step>',
+              '        <step_name>step1</step_name>',
+              '        <step_intent>intent1</step_intent>',
+              '        <system_prompt></system_prompt>',
+              '        <model_api>api1</model_api>',
+              "        <input_parameters>['param1']</input_parameters>",
+              '        <output>output1</output>',
+              '        <reasoning>reasoning1</reasoning>',
+              '        <is_list_output>False</is_list_output>',
+              '      </plan_step>',
+              '      <plan_step>',
+              '        <step_name>step2</step_name>',
+              '        <step_intent>intent2</step_intent>',
+              '        <system_prompt></system_prompt>',
+              '        <model_api>api2</model_api>',
+              "        <input_parameters>['param2']</input_parameters>",
+              '        <output>output2</output>',
+              '        <reasoning>reasoning2</reasoning>',
+              '        <is_list_output>False</is_list_output>',
+              '      </plan_step>',
+              '    </parallel>',
+              '  </plan_steps>',
+              '</plan>',
+          ],
+      ),
+      (
+          'mixed_sequential_and_parallel',
+          [
+              opal_plan_step.OpalPlanStep(
+                  step_name='step1',
+                  step_intent='intent1',
+                  model_api='api1',
+                  input_parameters=['param1'],
+                  output='output1',
+                  reasoning='reasoning1',
+              ),
+              [
+                  opal_plan_step.OpalPlanStep(
+                      step_name='step2',
+                      step_intent='intent2',
+                      model_api='api2',
+                      input_parameters=['param2'],
+                      output='output2',
+                      reasoning='reasoning2',
+                  ),
+                  opal_plan_step.OpalPlanStep(
+                      step_name='step3',
+                      step_intent='intent3',
+                      model_api='api3',
+                      input_parameters=['param3'],
+                      output='output3',
+                      reasoning='reasoning3',
+                  ),
+              ],
+              opal_plan_step.OpalPlanStep(
+                  step_name='step4',
+                  step_intent='intent4',
+                  model_api='api4',
+                  input_parameters=['param4'],
+                  output='output4',
+                  reasoning='reasoning4',
+              ),
+          ],
+          [
+              '<plan>',
+              '  <plan_name>test_plan</plan_name>',
+              '  <plan_steps>',
+              '    <plan_step>',
+              '      <step_name>step1</step_name>',
+              '      <step_intent>intent1</step_intent>',
+              '      <system_prompt></system_prompt>',
+              '      <model_api>api1</model_api>',
+              "      <input_parameters>['param1']</input_parameters>",
+              '      <output>output1</output>',
+              '      <reasoning>reasoning1</reasoning>',
+              '      <is_list_output>False</is_list_output>',
+              '    </plan_step>',
+              '    <parallel>',
+              '      <plan_step>',
+              '        <step_name>step2</step_name>',
+              '        <step_intent>intent2</step_intent>',
+              '        <system_prompt></system_prompt>',
+              '        <model_api>api2</model_api>',
+              "        <input_parameters>['param2']</input_parameters>",
+              '        <output>output2</output>',
+              '        <reasoning>reasoning2</reasoning>',
+              '        <is_list_output>False</is_list_output>',
+              '      </plan_step>',
+              '      <plan_step>',
+              '        <step_name>step3</step_name>',
+              '        <step_intent>intent3</step_intent>',
+              '        <system_prompt></system_prompt>',
+              '        <model_api>api3</model_api>',
+              "        <input_parameters>['param3']</input_parameters>",
+              '        <output>output3</output>',
+              '        <reasoning>reasoning3</reasoning>',
+              '        <is_list_output>False</is_list_output>',
+              '      </plan_step>',
+              '    </parallel>',
+              '    <plan_step>',
+              '      <step_name>step4</step_name>',
+              '      <step_intent>intent4</step_intent>',
+              '      <system_prompt></system_prompt>',
+              '      <model_api>api4</model_api>',
+              "      <input_parameters>['param4']</input_parameters>",
+              '      <output>output4</output>',
+              '      <reasoning>reasoning4</reasoning>',
+              '      <is_list_output>False</is_list_output>',
+              '    </plan_step>',
+              '  </plan_steps>',
+              '</plan>',
+          ],
+      ),
+  )
+  def test_render(self, plan_steps, expected_substrings):
+    plan = opal_plan.OpalPlan(
+        plan_name='test_plan',
+        plan_steps=plan_steps,
+    )
+    rendered = plan.render
+    self.assertCountEqual(rendered.splitlines(), expected_substrings)
+
+
+if __name__ == '__main__':
+  absltest.main()
